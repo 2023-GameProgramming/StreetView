cea685dc64fe7df952913005b3f9d325fe669151
tjdvlf2880 <71554146+HongSeongPil@users.noreply.github.com>
2023-05-26 15:34:50 +0900

    아바타 및 버튼생성

---
+++ Assets/Scripts/GoGwagidae.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoGwagidae : MonoBehaviour
+{
+    public void ChangeScene(string InScene2)
+    {
+        SceneManager.LoadScene("InScene2");
+    }
+
+}
